8555c33dda524cd2f95b82a9a5520c3a14db9b73
yanghao <1586076641@qq.com>
2018-01-31 18:10:36 +0800

    04

--- Day_03_03抽象类和接口/Test.kt
+++ Day_03_03抽象类和接口/Test.kt
@@ -1,9 +1,15 @@
 package Day_03_03抽象类和接口
 
+/**
+ *
+ * 新增关键字  is   查看是否是子类或者接口实现类
+ */
 fun main(args: Array<String>) {
     var man = worker("小明",18)
     man.speak()
     man.work()
     man.setname("小花")
     man.work()
+    println(man is human)  //结果为true
+    println(man is Imen) //结果为true
 }
